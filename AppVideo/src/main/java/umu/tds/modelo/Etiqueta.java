c3f5b57719a1da3bf9c062328988da3366e67032
Silvia <silvia.perezr@um.es>
2021-10-25 19:08:56 +0200

    Creacion de la clase etiqueta (get, hashCode, equals)

---
+++ AppVideo/src/main/java/umu/tds/modelo/Etiqueta.java
@@ -0,0 +1,39 @@
+package umu.tds.modelo;
+
+import java.util.Objects;
+
+public class Etiqueta {
+	//Atributos
+	private String nombre;
+	
+	//Constructor
+	private Etiqueta(String nombre) {
+		this.nombre = nombre;
+	}
+
+	//Get
+	public String getNombre() {
+		return nombre;
+	}
+
+	//HashCode & equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		
+		if (obj == null) return false;
+		
+		if (getClass() != obj.getClass()) return false;
+		
+		Etiqueta other = (Etiqueta) obj;
+		
+		return Objects.equals(nombre, other.nombre);
+	}
+	
+	
+}
