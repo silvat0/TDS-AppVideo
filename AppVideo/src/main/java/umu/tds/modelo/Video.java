4e70c5e2fb23d17540a195b839379403a6993edf
fschzg <felipe.sanchezg@um.es>
2021-10-26 13:52:46 +0200

    Etiquetas --> HashSet - Constructor --> titulo - Metodo añadir etiquetas

--- AppVideo/src/main/java/umu/tds/modelo/Video.java
+++ AppVideo/src/main/java/umu/tds/modelo/Video.java
@@ -1,13 +1,37 @@
 package umu.tds.modelo;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Video {
 	private String url;
 	private String titulo;
-	private Etiqueta etiq;
+	private HashSet<Etiqueta> etiqs;
 	
-	private Video(String titulo, Etiqueta etiq) {
+	private Video(String titulo) {
 		this.titulo = titulo;
-		this.etiq = etiq;
+		this.etiqs = new HashSet<>();
 		
 	}
+	
+	//Gets
+	public String getUrl() {
+		return url;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public Set<Etiqueta> getEtiq() {
+		return Collections.unmodifiableSet(etiqs);
+	}
+		
+	//Metodos
+	//1º metodo --> añadir etiquetas al video
+	public boolean addEtiqueta(Etiqueta...eti){
+		return etiqs.addAll(etiqs);
+	}
+	
 }
