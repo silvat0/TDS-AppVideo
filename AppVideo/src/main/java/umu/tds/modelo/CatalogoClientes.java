3cb4f8d08e0540b68c292d8572babc0299cc1093
Silvia <silvia.perezr@um.es>
2021-11-01 18:41:13 +0100

    Creacion de catalogo clientes con metodos basicos

---
+++ AppVideo/src/main/java/umu/tds/modelo/CatalogoClientes.java
@@ -0,0 +1,52 @@
+package umu.tds.modelo;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class CatalogoClientes {
+	
+	//Atributos 
+	private Map<String, Usuario> usuarios;
+	
+	//Construcctor
+	private CatalogoClientes() {
+		this.usuarios = new HashMap<>();
+	}
+	
+	//Metodos
+	//1º metodo --> Devolver una lista con todos los usuarios
+	public List<Usuario> getUsuarios() {
+		LinkedList<Usuario> lista = new LinkedList<>();
+		for(Usuario u : usuarios.values()) {
+			lista.add(u);
+		}
+		
+		return lista;
+	}
+	
+	//2º metodo --> Devolver un solo usuario por su alias
+	public Usuario getUsusario(String alias) {
+		return usuarios.get(alias);
+	}
+	
+	//3º metodo --> Devolver un solo usuario por su correo
+	public Usuario getUsuario(String correo) {
+		for(Usuario u : usuarios.values()) {
+			if (u.getEmail().equals(correo)) return u;
+		}
+		
+		return null;
+	}
+	
+	//4º metodo --> añadir un cliente al catalogo
+	public void addUsuario(Usuario user) {
+		usuarios.put(user.getUsuario(), user);
+	}
+	
+	//5º metodo --> eliminar a un cliente del catalago
+	public void removeUsuario(Usuario user) {
+		usuarios.remove(user.getUsuario());
+	}
+}
