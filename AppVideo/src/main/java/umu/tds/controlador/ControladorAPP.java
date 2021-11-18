4ab0745a2b9bd1129abfa91f1dd84be959e09c22
Silvia <silvia.perezr@um.es>
2021-11-19 00:17:59 +0100

    Implementado el patron de diseño singleton

--- AppVideo/src/main/java/umu/tds/controlador/ControladorAPP.java
+++ AppVideo/src/main/java/umu/tds/controlador/ControladorAPP.java
@@ -1,17 +1,34 @@
 package umu.tds.controlador;
 
+import java.time.LocalDate;
+
 import umu.tds.modelo.CatalogoUsuario;
 import umu.tds.modelo.Usuario;
 
 public class ControladorAPP {
 	
+	//Implementacion singleton
+	private static ControladorAPP unicaInstancia = null;
+	
+	//Atributos
 	private CatalogoUsuario cu;
 	
+	//Constructor
+	private ControladorAPP() {
+		cu = CatalogoUsuario.getInstancia();
+	}
 	
 	
+	//METODOS
 	
+	//Implementacion singleton
+	public static ControladorAPP getInstancia() {
+		if (unicaInstancia == null)
+			unicaInstancia = new ControladorAPP();
+		return unicaInstancia;
+	}
 	
-	
+	//Para el login
 	public boolean login(String user, String password) {
 		
 		Usuario u = cu.getUsusario(user);
@@ -21,4 +38,14 @@ public class ControladorAPP {
 		
 		return true;
 	}
+	
+	//Para el registro
+	public Usuario registro(String user, String pass, String email, String birth, String nombre, String ape){
+		
+		LocalDate ld = LocalDate.now();
+		Usuario u = new Usuario(nombre, email, ld, pass, ape, user);
+		return u;
+	}
+	
+	
 }
