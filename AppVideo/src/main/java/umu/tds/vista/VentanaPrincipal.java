2e33c55762178cb7a5f6363049d277ce245ff689
silvat0 <silvia.perezr@um.es>
2021-12-27 01:23:40 +0100

    ventana principla

--- AppVideo/src/main/java/umu/tds/vista/VentanaPrincipal.java
+++ AppVideo/src/main/java/umu/tds/vista/VentanaPrincipal.java
@@ -14,6 +14,12 @@ import java.awt.Font;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JFormattedTextField;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.CardLayout;
 
 public class VentanaPrincipal {
 
@@ -47,75 +53,110 @@ public class VentanaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 782, 490);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{30, 0, 0, 0, 30};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_panel.rowWeights = new double[]{0.0, 1.0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0};
 		panel.setLayout(gbl_panel);
 		
-		JLabel lblNewLabel = new JLabel("MI PERFIL");
-		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 16));
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.gridwidth = 2;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.anchor = GridBagConstraints.NORTH;
-		gbc_lblNewLabel.gridx = 2;
-		gbc_lblNewLabel.gridy = 1;
-		panel.add(lblNewLabel, gbc_lblNewLabel);
+		JPanel panel_arriba = new JPanel();
+		panel_arriba.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		GridBagConstraints gbc_panel_arriba = new GridBagConstraints();
+		gbc_panel_arriba.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_arriba.fill = GridBagConstraints.BOTH;
+		gbc_panel_arriba.gridx = 1;
+		gbc_panel_arriba.gridy = 1;
+		panel.add(panel_arriba, gbc_panel_arriba);
+		GridBagLayout gbl_panel_arriba = new GridBagLayout();
+		gbl_panel_arriba.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_arriba.rowHeights = new int[]{0, 0};
+		gbl_panel_arriba.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_arriba.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_arriba.setLayout(gbl_panel_arriba);
 		
-		JLabel lblNewLabel_1 = new JLabel("Nickname:");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 2;
-		gbc_lblNewLabel_1.gridy = 3;
-		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		JButton btnNewButton = new JButton("Registro");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 12;
+		gbc_btnNewButton.gridy = 0;
+		panel_arriba.add(btnNewButton, gbc_btnNewButton);
 		
-		JLabel nicknameValue = new JLabel("null");
-		GridBagConstraints gbc_nicknameValue = new GridBagConstraints();
-		gbc_nicknameValue.anchor = GridBagConstraints.WEST;
-		gbc_nicknameValue.insets = new Insets(0, 0, 5, 5);
-		gbc_nicknameValue.gridx = 3;
-		gbc_nicknameValue.gridy = 3;
-		panel.add(nicknameValue, gbc_nicknameValue);
+		JButton btnNewButton_1 = new JButton("Login");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_1.gridx = 13;
+		gbc_btnNewButton_1.gridy = 0;
+		panel_arriba.add(btnNewButton_1, gbc_btnNewButton_1);
 		
-		JLabel lblNewLabel_3 = new JLabel("Email:");
-		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_3.gridx = 2;
-		gbc_lblNewLabel_3.gridy = 4;
-		panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		JButton btnNewButton_2 = new JButton("Logout");
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_2.gridx = 15;
+		gbc_btnNewButton_2.gridy = 0;
+		panel_arriba.add(btnNewButton_2, gbc_btnNewButton_2);
 		
-		JLabel frmtdtxtfldXxxx = new JLabel();
-		frmtdtxtfldXxxx.setText("xxxx");
-		GridBagConstraints gbc_frmtdtxtfldXxxx = new GridBagConstraints();
-		gbc_frmtdtxtfldXxxx.anchor = GridBagConstraints.WEST;
-		gbc_frmtdtxtfldXxxx.insets = new Insets(0, 0, 5, 5);
-		gbc_frmtdtxtfldXxxx.gridx = 3;
-		gbc_frmtdtxtfldXxxx.gridy = 4;
-		panel.add(frmtdtxtfldXxxx, gbc_frmtdtxtfldXxxx);
+		JButton btnNewButton_3 = new JButton("Premium");
+		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
+		gbc_btnNewButton_3.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_3.gridx = 17;
+		gbc_btnNewButton_3.gridy = 0;
+		panel_arriba.add(btnNewButton_3, gbc_btnNewButton_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("Fecha de nacimiento:");
-		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-		gbc_lblNewLabel_4.gridwidth = 2;
-		gbc_lblNewLabel_4.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_4.gridx = 1;
-		gbc_lblNewLabel_4.gridy = 5;
-		panel.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		JPanel panel_centro = new JPanel();
+		frame.getContentPane().add(panel_centro, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_centro = new GridBagLayout();
+		gbl_panel_centro.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_centro.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_centro.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_centro.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		panel_centro.setLayout(gbl_panel_centro);
 		
-		JLabel lblNewLabel_2 = new JLabel("xxxx");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_2.gridx = 3;
-		gbc_lblNewLabel_2.gridy = 5;
-		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		JButton btnNewButton_4 = new JButton("Explorar");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
+		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_4.gridx = 1;
+		gbc_btnNewButton_4.gridy = 3;
+		panel_centro.add(btnNewButton_4, gbc_btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Mis listas");
+		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
+		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_5.gridx = 2;
+		gbc_btnNewButton_5.gridy = 3;
+		panel_centro.add(btnNewButton_5, gbc_btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("Recientes");
+		GridBagConstraints gbc_btnNewButton_6 = new GridBagConstraints();
+		gbc_btnNewButton_6.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_6.gridx = 3;
+		gbc_btnNewButton_6.gridy = 3;
+		panel_centro.add(btnNewButton_6, gbc_btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("Nueva lista");
+		GridBagConstraints gbc_btnNewButton_7 = new GridBagConstraints();
+		gbc_btnNewButton_7.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_7.gridx = 4;
+		gbc_btnNewButton_7.gridy = 3;
+		panel_centro.add(btnNewButton_7, gbc_btnNewButton_7);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.gridwidth = 10;
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 1;
+		gbc_panel_1.gridy = 4;
+		panel_centro.add(panel_1, gbc_panel_1);
+		panel_1.setLayout(new CardLayout(0, 0));
 	}
 
 }
