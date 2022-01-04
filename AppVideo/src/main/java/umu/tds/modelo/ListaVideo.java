ea7e5fe83037a1d516c384d2bec99f94c49f872e
felpud0 <felipe.sanchezg@um.es>
2022-01-04 18:14:42 +0100

    añadido equals

--- AppVideo/src/main/java/umu/tds/modelo/ListaVideo.java
+++ AppVideo/src/main/java/umu/tds/modelo/ListaVideo.java
@@ -3,6 +3,7 @@ package umu.tds.modelo;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class ListaVideo {
 	//Atributos
@@ -47,5 +48,21 @@ public class ListaVideo {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ListaVideo other = (ListaVideo) obj;
+		return Objects.equals(nombre, other.nombre);
+	}
 }
