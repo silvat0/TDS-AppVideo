5f64bc1c9adaa46c44ae3a4b37ba72c064961084
felpud0 <felipe.sanchezg@um.es>
2024-06-13 02:33:41 +0200

    definidas e instaladas las dependencias de los binarios

---
+++ AppVideo/jars-install.sh
@@ -0,0 +1,5 @@
+mvn install:install-file -DlocalRepositoryPath=repo -Dfile=lib/CargadorVideos.jar -DgroupId=umu.tds -DartifactId=CargadorVideos -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -DlocalRepositoryPath=repo -Dfile=lib/DriverPersistencia.jar -DgroupId=umu.tds -DartifactId=DriverPersistencia -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -DlocalRepositoryPath=repo -Dfile=lib/Luz.jar -DgroupId=umu.tds -DartifactId=Luz -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -DlocalRepositoryPath=repo -Dfile=lib/VideoWeb.jar -DgroupId=umu.tds -DartifactId=VideoWeb -Dversion=1.0.0 -Dpackaging=jar
+
